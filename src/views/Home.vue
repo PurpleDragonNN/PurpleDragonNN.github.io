ee6c4cd6aa4592320ecbbfc6e61fa1e24c71ea27
PurpleDragonNN <576817275@qq.com>
2021-04-26 01:30:52 +0800

    feat：词云图

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,12 @@
       <p class="title">圣越32产业园</p>
     </div>
     <div class="main">
-      <div class="main-left"></div>
+      <div class="main-left">
+        <word-cloud-chart
+            id="echarts05"
+            :data="echarts05Data"
+        />
+      </div>
       <div class="main-middle"></div>
       <div class="main-right"></div>
     </div>
@@ -13,10 +18,12 @@
 
 <script>
 import bordeBox from '@/components/bordeBox.vue'
+import WordCloudChart from '@/components/WordCloudChart'
 import 'echarts-liquidfill/src/liquidFill.js'
 export default {
   name: 'Home',
   components: {
+    WordCloudChart
   },
   data () {
     return {
@@ -26,57 +33,118 @@ export default {
       toolTipContent: {},
       nowTime: '',
       activeVideo: '',
-      isShowVideoDialog: false
+      isShowVideoDialog: false,
+      echarts05Data: [
+        {
+          name: "吴亦凡",
+          textStyle: {
+            // Color can be a callback function or a color string
+            color: '#00eaff'
+          },
+          value: 15000
+        },
+        {
+          name: "B站",
+          value: 10081
+        },
+        {
+          name: "微博",
+          value: 9386
+        },
+        {
+          name: "汽车之家",
+          value: 7500
+        },
+        {
+          name: "旭子工作室",
+          value: 7500
+        },
+        {
+          name: "抖音",
+          value: 6500
+        },
+        {
+          name: "老司机出品",
+          value: 6500
+        },
+        {
+          name: "懂车帝",
+          value: 6000
+        },
+        {
+          name: "合肥庐阳保时捷中心",
+          value: 4500
+        },
+        {
+          name: "老司机汽车",
+          value: 3800
+        },
+        {
+          name: "萝卜报告",
+          value: 3000
+        },
+        {
+          name: "易车网",
+          value: 2500
+        },
+        {
+          name: "韩路",
+          value: 2300
+        },
+        {
+          name: "ranta",
+          value: 2000
+        },
+        {
+          name: "萝卜报告",
+          value: 1900
+        },
+        {
+          name: "现代化经济体系",
+          value: 1800
+        },
+        /*{
+          name: "国防动员",
+          value: 1700
+        },
+        {
+          name: "信息化战争",
+          value: 1600
+        },
+        {
+          name: "局部战争",
+          value: 1500
+        },
+        {
+          name: "教育",
+          value: 1200
+        },
+        {
+          name: "职业教育",
+          value: 1100
+        },
+        {
+          name: "兵法",
+          value: 900
+        },
+        {
+          name: "一国两制",
+          value: 800
+        },
+        {
+          name: "特点社会主义思想",
+          value: 700
+        },*/
+      ]
     }
   },
   mounted () {
     this.resizeFn()
     window.onresize = this.resizeFn
-    this.getEchartData()
-    this.videoList.forEach(item => {
-      this.$refs[item.name][0].addEventListener('play', () => {
-        item.isPlay = true
-      }, false);
-    })
-    setInterval(() => {
-      let date = new Date()
-      let week = new Date().getDay()
-      let weekObj = {
-        0: '天',
-        1: '一',
-        2: '二',
-        3: '三',
-        4: '四',
-        5: '五',
-        6: '六',
-      }
-      let month = date.getMonth()<10 ? '0'+(date.getMonth()+1) : date.getMonth()
-      this.nowTime = `${month}-${date.getDate()}-${date.getFullYear()} 星期${weekObj[week]} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`
-    },1000)
+
+    // this.echarts05Data.
   },
   methods: {
-    btnEvent () {
-      this.$router.push({path: './second'})
-    },
-    play (val){
-      val.isPlay = true
-      this.$refs[val.name][0].play()
-    },
-    closeVideo () {
-      this.isCloseAll = true
-      this.videoList.forEach(item => {
-        item.isPlay = false
-      })
-      this.$nextTick(() => {
-        this.isCloseAll = false
-      })
-    },
-    getEchartData () {
-      this.yuchangfenData()
-      this.roujiData()
-      this.youjifeiData()
-      this.fuhuaData()
-    },
     // 自适应
     resizeFn () {
       let win_w = document.documentElement.clientWidth ||  document.body.clientWidth
