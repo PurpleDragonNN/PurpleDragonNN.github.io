2673a179cea544f0d1d25d0513d174bc7233511b
yangzilong <yangzilong@do1.com.cn>
2021-04-27 16:57:23 +0800

    feat: 终版2

--- src/App.vue
+++ src/App.vue
@@ -6,11 +6,16 @@
 
 <style lang="scss">
 @import "../src/assets/css/reset.css";
+@font-face {
+  font-family: 'PorscheNext-Regular';
+  src: url("./assets/font/PorscheNext-Regular.otf");
+}
 html{
   width: 100%;
   height: 100%;
 }
 body{
+  font-family: 'PorscheNext-Regular', 'SimHei',Microsoft YaHei;
   width: 100%;
   height: 100%;
   #app{
